ad2055802470af74a9c9d544eabf71d696be2b41
Dieko <admeijers@hotmail.nl>
2019-05-24 13:11:40 +0200

    Navbar en uikit shit die berend niet heeft gestuurd gvd navbar is te vinden in home voor nu

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,9 @@
 <html lang="nl">
-<head><title>BDRLaravel Project</title></head>
+<head>
+    <title>BDRLaravel Project</title>
+    <link rel="stylesheet" href="../css/uikit.css">
+    <link rel="stylesheet" href="../js/uikit.js">
+</head>
 <body>
 @yield('header')
 @yield('content')
