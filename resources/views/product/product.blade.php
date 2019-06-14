7ba62f76b4ef8800c966db2e9f4ce5f449f5525d
berendkorendijk <35295795+berendkorendijk@users.noreply.github.com>
2019-06-14 14:34:36 +0200

    basis layout voor de producten, button is nog niet een ahref die werkt daarvan kunnen we een product page voor maken en producten veranderen naar assortiment

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -5,7 +5,32 @@
 @endsection
 
 @section('content')
-    this is content
+    <br>
+    <div class="uk-child-width-1-3@m" uk-grid>
+        <div>
+            <div class="uk-card-small uk-card-default">
+                <div class="uk-card-body">
+                    <h3 class="uk-card-title">Hoofd ding Iets</h3>
+                    <p>d tempor incididunt.</p>
+                </div>
+                <div class="uk-card-media-top uk-width-1-2 uk-margin-left">
+                    <img src="{{asset('images/oudepf.jpg')}}" >
+                </div>
+            </div>
+        </div>
+        <div>
+            <div class="uk-card-small uk-card-default">
+                <div class="uk-card-body">
+                    <h3 class="uk-card-title">Description</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+
+                    <div class="uk-margin-left">
+                        <a class="uk-button uk-button-default uk-margin-auto-left" href="#">more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
